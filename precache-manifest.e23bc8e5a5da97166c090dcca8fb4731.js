2ae2be160204bbe98782dfa69b66cb4dca0a3a35
PedroMarqDev <PedroMarqDev@gmail.com>
2021-01-23 18:19:09 -0300

    Updates

---
+++ precache-manifest.e23bc8e5a5da97166c090dcca8fb4731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8fd93112694370e0c6b7b791fe2664",
+    "url": "/walletredux/index.html"
+  },
+  {
+    "revision": "b78c69d37481ad8e67c7",
+    "url": "/walletredux/static/css/main.cbc02002.chunk.css"
+  },
+  {
+    "revision": "425bb1a48e7506a2407e",
+    "url": "/walletredux/static/js/2.5cd90c2b.chunk.js"
+  },
+  {
+    "revision": "df34426e3c67cba54df30dae4655a86f",
+    "url": "/walletredux/static/js/2.5cd90c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b78c69d37481ad8e67c7",
+    "url": "/walletredux/static/js/main.fa3a12bb.chunk.js"
+  },
+  {
+    "revision": "cfec520f339885b7d45a",
+    "url": "/walletredux/static/js/runtime-main.8687d673.js"
+  }
+]);
