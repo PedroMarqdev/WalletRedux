2e0475a809c8e883094ec6262191ade659973b5d
PedroMarqDev <PedroMarqDev@gmail.com>
2021-01-23 18:42:05 -0300

    Updates

---
+++ precache-manifest.56f315b6d269d50aa39b9c981ae3b6d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf9e80dadf4be7fddc425c0bbe58c686",
+    "url": "/walletredux/login/index.html"
+  },
+  {
+    "revision": "e0b078f953d7757fed07",
+    "url": "/walletredux/login/static/css/main.cbc02002.chunk.css"
+  },
+  {
+    "revision": "425bb1a48e7506a2407e",
+    "url": "/walletredux/login/static/js/2.5cd90c2b.chunk.js"
+  },
+  {
+    "revision": "df34426e3c67cba54df30dae4655a86f",
+    "url": "/walletredux/login/static/js/2.5cd90c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b078f953d7757fed07",
+    "url": "/walletredux/login/static/js/main.ee3426c1.chunk.js"
+  },
+  {
+    "revision": "4c4d5552e872219ddd1e",
+    "url": "/walletredux/login/static/js/runtime-main.a0b1a99a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/walletredux/precache-manifest.82fd9a4619d24b40ce753b8810e4596d.js"
+  "/walletredux/login/precache-manifest.56f315b6d269d50aa39b9c981ae3b6d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/walletredux/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/walletredux/login/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
